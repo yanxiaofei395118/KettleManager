47ae94cd9cd20bacdf4de05a296342e057e51ffc
大鹏 <gudepeng@qq.com>
2017-09-04 14:06:47 +0800

    修改了一些注解
    修改了一些注解

--- src/main/java/com/gdp/km/Application.java
+++ src/main/java/com/gdp/km/Application.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+	/**
+	 * 程序启动方法
+	 * */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
--- src/main/java/com/gdp/km/core/config/ApplicationFilterconfig.java
+++ src/main/java/com/gdp/km/core/config/ApplicationFilterconfig.java
@@ -6,18 +6,24 @@ import org.springframework.context.annotation.Configuration;
 
 import com.gdp.km.core.filter.CrossOriginFilter;
 
+/**
+ * 创建cors过滤器
+ * */
 @Configuration
 public class ApplicationFilterconfig {
 	
 	/**
-	 * 创建cors过滤器
+	 * @see setFilter 设置该过滤器类方法
+	 * @see setName 设置该过滤器名称
+	 * @see addUrlPatterns 设置该过滤器过滤的url
+	 * @see setOrder 设置该过滤器的执行顺序
 	 */
 	@Bean
 	public FilterRegistrationBean MyFilterRegistration() {
 		FilterRegistrationBean registration = new FilterRegistrationBean();
 		registration.setFilter(new CrossOriginFilter());
-		registration.addUrlPatterns("/*");
 		registration.setName("CrossOriginFilter");
+		registration.addUrlPatterns("/*");	
 		registration.setOrder(-Integer.MAX_VALUE);
 		return registration;
 	}
